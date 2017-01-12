a7967c31fa599900f429bb53eb4d1e1805b5d484
Gan-Jiang <yl2464@cornell.edu>
2017-01-12 12:35:42 -0500

    complete camera calibration
    the camera matrix and distortion coefficients are stored in
    dist_pickle.p as dist_pickle[“mtx”] and dist_pickle[“distilled”]

---
+++ calibration.py
@@ -0,0 +1,48 @@
+import numpy as np
+import cv2
+import glob
+import matplotlib.pyplot as plt
+import pickle
+
+
+#Compute the camera calibration matrix and distortion coefficients given a set of chessboard images.
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*8,3), np.float32)
+objp[:,:2] = np.mgrid[0:8, 0:6].T.reshape(-1,2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d points in real world space
+imgpoints = [] # 2d points in image plane.
+
+# Make a list of calibration images
+images = glob.glob('camera_cal/GO*.jpg')
+
+# Step through the list and search for chessboard corners
+for idx, fname in enumerate(images):
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Find the chessboard corners
+    ret, corners = cv2.findChessboardCorners(gray, (8,6), None)
+
+    # If found, add object points, image points
+    if ret == True:
+        objpoints.append(objp)
+        imgpoints.append(corners)
+
+# Test undistortion on an image
+img = cv2.imread('test_images/test4.jpg')
+img_size = (img.shape[1], img.shape[0])
+
+# Do camera calibration given object points and image points
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size,None,None)
+
+
+dst = cv2.undistort(img, mtx, dist, None, mtx)
+cv2.imwrite('test_undist.jpg',dst)
+
+# Save the camera calibration result for later use (we won't worry about rvecs / tvecs)
+dist_pickle = {}
+dist_pickle["mtx"] = mtx
+dist_pickle["dist"] = dist
+pickle.dump( dist_pickle, open("dist_pickle.p", "wb" ) )
